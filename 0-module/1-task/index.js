bf506b62116ceccf787f7fe66bdea45d17de834d
Snizhana-Uzumaki <yukitanaka8525@gmail.com>
2023-02-09 20:01:04 +0100

    Completed the first task

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sum(m, n) {
-  // ваш код...
+  return m + n;
 }
+
+number_two = prompt('m=');
+let m = Number(number_two);
+number_one = prompt('n=');
+let n = Number(number_one);
+
+alert(sum(m, n));
