89acde4d12756f0622cd950f66dace6f59384396
Snizhana-Uzumaki <yukitanaka8525@gmail.com>
2023-02-15 09:17:58 +0100

    Fix program

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,16 +1,15 @@
-/**
- * Эту функцию трогать не нужно
- */
+
 function print(text) {
   console.log(text);
 }
 
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
 function isValid(name) {
-  // ваш код...
+  if ( !name || name.length < 4 || name.indexOf(' ') != -1) {
+    return false  
+  } 
+  else {
+    return true
+  }
 }
 
 function sayHello() {
