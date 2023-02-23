a07431c84d30307bf9358725003395b3f6601938
Snizhana-Uzumaki <yukitanaka8525@gmail.com>
2023-02-23 11:27:53 +0100

    Task 1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary(salaries) {  
+  let sum=0;
+  let SalariesKeys = Object.keys(salaries);
+  for(let i = 0; i <= SalariesKeys.length - 1; i++) {
+    if(typeof salaries[SalariesKeys[i]]==='number' && !Number.isNaN(salaries[SalariesKeys[i]]) && Number.isFinite(salaries[SalariesKeys[i]])){
+      sum+=salaries[SalariesKeys[i]];
+    }
+  }
+  return sum;
 }
