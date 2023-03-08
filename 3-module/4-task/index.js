8a66066bb0df3b3c27b84816f9580753fc6a5bc5
Snizhana-Uzumaki <yukitanaka8525@gmail.com>
2023-03-08 12:41:38 +0100

    Task 4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,23 +1,5 @@
 
 function showSalary(users, age) {
-  let users = [user1, user2];
-  let ArrUserAgeIsTrue = users.filter(item => {if (item.age <= age) return item;})
-  let ArrNameAndBalance = ArrUserAgeIsTrue.map(item => {
-    let i = {
-      name: '',
-      balance: 0,
-    };
-    i.name = item.name;
-    i.balance = item.balance;
-    return i;
-  } );
-  let ArrWithoutObj = ArrNameAndBalance.map(item => {
-    return Object.values(item);
-  })
-  let NewStr = '';
-  for (let i = 0; i <  ArrWithoutObj.length; i++) {
-    NewStr += ArrWithoutObj[i].join(',') + '\r\n'
-  } 
-    console.log(NewStr);
+  return users.filter(u => u.age <= age).map(u => `${u.name}, ${u.balance}`).join('\r\n')
  }
 
