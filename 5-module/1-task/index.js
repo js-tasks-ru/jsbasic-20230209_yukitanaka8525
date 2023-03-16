f94d75f983576e7ba1784a13c616eecdbc57648f
Snizhana-Uzumaki <yukitanaka8525@gmail.com>
2023-03-16 11:49:57 +0100

    описание того, что вы сделали

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,5 @@
-function hideSelf() {
-  // ваш код...
+function hideSelf() { 
+  document.querySelector('.hide-self-button').onclick = function() {
+    document.querySelector('.hide-self-button').hidden = true;
+  }
 }
