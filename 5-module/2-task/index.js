18b115ff81abf3d1eb6f444d0d0684367061936b
Snizhana-Uzumaki <yukitanaka8525@gmail.com>
2023-03-17 12:24:25 +0100

    Module 5 task 2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  let x = document.getElementById("text");
+  document.querySelector(".toggle-text-button").onclick = function(){
+  if (window.getComputedStyle(x, null).display === "none") {
+    x.hidden = false;
+  }
+  else {
+    x.hidden = true;
+  }
+}
 }
